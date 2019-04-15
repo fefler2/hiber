bb9824aec2570eae387dd9ee501f0c7738d668b6
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-15 11:01:58 +0200

    dodanie getterow i setterow

--- src/main/java/Hiber/Customer.java
+++ src/main/java/Hiber/Customer.java
@@ -29,4 +29,26 @@ public class Customer implements Serializable{
     @Column(name = "lastName", nullable = false) // kolumna
     private String lName;
 
+    public int getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getfName(){
+        return fName;
+    }
+    public void setfName(String fName){
+        this.fName =fName;
+    }
+    public String getlName(){
+        return lName;
+    }
+    public void setlName(String fName){
+        this.lName = lName;
+    }
+
+
 }
