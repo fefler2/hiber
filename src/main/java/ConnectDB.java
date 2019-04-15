ddcf933af4b073e0340736e0f97bc3e982e4c396
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-15 12:04:23 +0200

    mySQL connection

---
+++ src/main/java/ConnectDB.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectDB {
+    // DB Connection variables
+
+    static Connection connection = null;
+    static String dataBaseName = "";
+    //
+    //    czy bez "/" na koncu static String url = "jdbc:mysql://localhost"
+//    static String url = "jdbc:mysql://localhost/" +dataBaseName;
+    static String url = "jdbc:mysql://localhost:3306/fussa?useLegacyDatetimeCode=false&serverTimezone=UTC\n" +dataBaseName;
+    static String username = "root";
+    static String password  = ""; // bez hasla?, chyba jest dobrze
+//    static String password  = ""; // bez hasla?
+
+
+
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        connection = DriverManager.getConnection(url, username, password);
+
+        String sql;
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO 'studendatabase', 'student' ('name') VALUES ('michael clark');");
+        int status = ps.executeUpdate();
+        if (status != 0) {
+            System.out.println("Database was Connection");
+            System.out.println("Record was inserted");
+        }
+
+    }
+}
