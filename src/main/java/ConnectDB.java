474293978828822b618bb0301863107bd1882563
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-15 12:41:09 +0200

    DZIALA!

--- src/main/java/ConnectDB.java
+++ src/main/java/ConnectDB.java
@@ -10,7 +10,7 @@ public class ConnectDB {
     static String dataBaseName = ""; // nazwa bazy danych
     //
     //    czy bez "/" na koncu static String url = "jdbc:mysql://localhost"
-    static String url = "jdbc:mysql://localhost" ;
+    static String url = "jdbc:mysql://localhost/studentdatabase" ;
 
 //     static String url = "jdbc:mysql://localhost:3306/fussa?useLegacyDatetimeCode=false&serverTimezone=UTC\n" +dataBaseName;
     static String username = "root";
@@ -27,7 +27,8 @@ public class ConnectDB {
         String sql;
 //        PreparedStatement ps = connection.prepareStatement("select * from student");
 //        System.out.println(ps.toString());
-        PreparedStatement ps = connection.prepareStatement("insert into student (studentID) values (6);");
+        // studentdatabase
+        PreparedStatement ps = connection.prepareStatement("insert into student (studentID) values (7);");
 //        INSERT INTO 'studendatabase', 'student' ('name') VALUES ('michael clark');
         int status = ps.executeUpdate();
 //        if (status != 0) {
