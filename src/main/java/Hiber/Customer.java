50c7e3517e53deacb95037ef133aaf8eff1bc000
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-15 10:59:38 +0200

    dodanie @Entity, @Id @Column

--- src/main/java/Hiber/Customer.java
+++ src/main/java/Hiber/Customer.java
@@ -1,9 +1,14 @@
 package Hiber;
 import java.io.Serializable;
+
+import javax.persistence.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity // which object should be persisted
+@Table(name = "customer")
 public class Customer implements Serializable{
 
 
@@ -11,5 +16,17 @@ public class Customer implements Serializable{
 
 
 
+    @Id
+    @Column(name = "custID", unique = true) // kolumna
+    private int id;
+
+    @Id
+    @Column(name = "firstName", nullable = false) // kolumna
+    private String fName;
+
+
+    @Id
+    @Column(name = "lastName", nullable = false) // kolumna
+    private String lName;
 
 }
